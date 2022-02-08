093aae0f9d1aa3dfb5c548cf6d8b2ef7d96bf2da
Ida <94110256+Ida-2@users.noreply.github.com>
2022-02-08 14:52:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+$(document).ready(function(){
 
+    // variables
+    $surface = $('.surface');
+    $car = $('.car');
+    $img = $('.car img');
+    let flag = true;
+
+    const cars = ['./assets/Img_05.PNG','./assets/Img_06.PNG'];
+
+    //key press 
+    $(document).on('keypress', function(e){
+        if(e.which == 39){
+            $($surface).toggleClass('moveRight');
+            $($car).toggleClass('suspension');
+        }
+    })
+
+    $(document).on('keypress', function(e){
+        if(e.which == 40){
+            if(flag){
+                flag = false;
+                $img.attr('src', cars[0]);
+            }else{
+                flag = true;
+                $img.attr('src', cars[1]);
+            }
+        }
+    })
+
+});
